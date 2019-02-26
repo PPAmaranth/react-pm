87bdb338bd1a983cb90deb5907a58970c025df17
PPAmaranth <523002128@qq.com>
2019-02-26 17:29:53 +0800

    Signed-off-by: PPAmaranth <523002128@qq.com>

--- config/config.js
+++ config/config.js
@@ -1,11 +1,31 @@
 export default {
 	plugins:[
 		['umi-plugin-react', {
-			
+			antd: true,
+      dva:true
 	    }],
 	],
 	routes: [{
     path: '/',
-    component: './main',
+    component: '../layout',
+    routes: [
+      {
+        path: '/',
+        component: './main'
+      },
+      { path: 'puzzlecards', component: './puzzlecards' },
+      {
+        path: 'main',
+        component: './main'
+      },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+        ]
+      },
+    ]
   }],
 };
